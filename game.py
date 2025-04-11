64f1ad5e9b52897889db9ef423a50c1a889ba117
ZhenHongOla <e0960559@u.nus.edu>
2025-04-11 14:30:37 +0800

    Update

--- game.py
+++ game.py
@@ -306,6 +306,7 @@ class Game:
             player = self.players["name"]
             if not player.alive:
                 alive_human_player -= 1
+        print(f"Alive human players: {alive_human_player}")
         if alive_human_player == 0:
             await self.send_announcement(f"All human players are eliminated! {next_player.name} wins!")
             self.game_record.finish_game(next_player.name)
